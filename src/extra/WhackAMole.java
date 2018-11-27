c2c31359f275238a4820652e043b65372895899e
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-26 17:41:54 -0800

    Whack a Mole

---
+++ src/extra/WhackAMole.java
@@ -0,0 +1,57 @@
+package extra;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class WhackAMole {
+	JFrame frame= new JFrame();
+	JPanel panel = new JPanel();
+	 	
+
+
+
+	WhackAMole(){
+		frame.add(panel);
+		frame.setVisible(true);
+		
+		frame.setSize(400, 400);
+		frame.setTitle("Whack A Button For Fame And Glory");
+		
+		
+		for (int i = 0; i < 24; i++) {
+			JButton button = new JButton();
+		panel.add(button);
+		
+		}
+		
+}
+public static void main(String[] args) {
+
+}	
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
