b8a840ce6f54f2c3030f1528d840815f867757c7
ilabstaff <ilabstaff@172.16.116.81>
2019-01-14 17:44:51 -0800

    Computer Practice Test

---
+++ src/extra/Computer.java
@@ -0,0 +1,39 @@
+package extra;
+
+public class Computer {
+
+	Computer(){
+		
+	}
+	
+	BullDozer(int hp){
+		this.horsePower = hp;
+		
+		
+	}
+	
+	
+	Ninja(int nots, String not){
+	
+		this.numberOfThrowingStars = nots;
+		this.nameOfTarget = not;
+	
+	}
+	
+	Building(String st, int nos, float h, boolean he){
+		
+		this.name = st;
+		this.numberOfStories = nos;
+		this.height = h;
+		this.hasElevator = he
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
