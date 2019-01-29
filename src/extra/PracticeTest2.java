b9a4219a4b3186df89eebb033d0a8397ffb87ab0
ilabstaff <ilabstaff@172.16.116.77>
2019-01-28 17:42:11 -0800

    Practice Test 2

---
+++ src/extra/PracticeTest2.java
@@ -0,0 +1,106 @@
+package extra;
+
+import javax.swing.JOptionPane;
+
+public class PracticeTest2 {
+
+	1.
+	Student s = new Student();
+	
+	2.
+	Oven o = new Oven();
+	
+	3. 	
+	Animal(){
+		
+	}
+	
+	4.
+	
+	JTextField
+	JButton
+	JPanel
+	JFrame
+	JLabel
+	
+	
+	5.
+	
+	a)frame.addMouseListener
+	
+	b)
+	
+	c)button.addActionListener();
+	
+	6.
+	
+	double Numbers(num 9, ber 7){
+		
+		double average = (num+ber)/2.0;
+		
+		return average;
+		
+		
+	}
+	
+	7.
+	
+	int sqaureNum(int 2) {
+		return 2;
+	}
+	
+	8.
+
+	Void Annoy(String "hey"){
+		
+		
+		for (int i = 0; i < 3; i++) {
+			
+		
+		JOptionPane.showMessageDialog(null, "hey");
+		}
+	}
+	
+	
+	9.
+	
+	
+	public class Smurf { String name;
+	
+	
+	
+	boolean wearsARedHat;
+	
+	
+	
+	Smurf(boolean wearsRedHat) { 
+		
+		this.wearsARedHat = wearsRedHat;
+	
+	}
+	
+	void setName(String name) {
+		
+		this.name = name;
+		
+		
+		
+		
+		Smurf s = new Smurf();
+		
+		s.setName("John");
+		
+	} 
+
+		}}
+
+10.
+The second if statement shoudnt have "if else" it should just be "if"
+The third if statement doesnt need the "if else" oit just needs to say "else"
+
+
+
+	
+	
+	
+
